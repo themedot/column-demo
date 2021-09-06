ee4c60c14f52a52c38ff5c139a2e47fbfebe1fff
Mahadi001 <sadat.himel@gmail.com>
2021-09-06 15:20:31 +0600

    add pages columns and show values

--- column-demo.php
+++ column-demo.php
@@ -26,15 +26,23 @@ function coldemo_post_columns($columns){
     // $columns['date'] = 'Date';
     // $columns['comments'] = 'Comments';
     $columns['id'] = __('Post ID','column-demo');
+    $columns['thumbnail'] = __('Thumbnail','column-demo');
     return $columns;
 }
 add_filter("manage_posts_columns","coldemo_post_columns");
+add_filter("manage_pages_columns","coldemo_post_columns");
 
 
 function coldemo_post_column_data($column,$post_id){
-    echo $post_id;
+    if('id' == $column){
+        echo $post_id;
+    }elseif('thumbnail' == $column){
+        $thumbnail = get_the_post_thumbnail($post_id,array(100,100));
+        echo $thumbnail;
+    }
 }
 add_action( 'manage_posts_custom_column', 'coldemo_post_column_data', 10, 2 );
+add_action( 'manage_pages_custom_column', 'coldemo_post_column_data', 10, 2 );
 
 
 
