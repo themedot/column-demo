42497982725063f5e976b3089d11afe67d052fcb
Mahadi001 <sadat.himel@gmail.com>
2021-09-06 11:01:52 +0600

    update

---
+++ column-demo.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Column demo
+Plugin URI: http://example.com/
+Description: 
+Version: 1.0
+Author: himel
+Author URI: http://example.com/
+License: GPLv2 or later
+Text Domain: 
+Domain Path: /languages
+*/
+
