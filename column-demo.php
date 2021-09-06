2d5a31e152ffc40f4eeba6a7f8e5ce2ebd3ca627
Mahadi001 <sadat.himel@gmail.com>
2021-09-06 13:31:53 +0600

    add new column post id

--- column-demo.php
+++ column-demo.php
@@ -7,7 +7,7 @@ Version: 1.0
 Author: himel
 Author URI: http://example.com/
 License: GPLv2 or later
-Text Domain: 
+Text Domain: column-demo
 Domain Path: /languages
 */
 
@@ -15,3 +15,52 @@ function coldemo_bootstrap(){
     load_plugin_textdomain("column-demo",false,dirname(__FILE__)."/languages");
 }
 add_action( 'plugin_loaded', 'coldemo_bootstrap' );
+
+function coldemo_post_columns($columns){
+    print_r($columns);
+    unset($columns['tags']);
+    unset($columns['comments']);
+    // unset($columns['author']);
+    // unset($columns['categories']);
+    // unset($columns['date']);
+    // $columns['date'] = 'Date';
+    // $columns['comments'] = 'Comments';
+    $columns['id'] = __('Post ID','column-demo');
+    return $columns;
+}
+add_filter("manage_posts_columns","coldemo_post_columns");
+
+
+function coldemo_post_column_data($column,$post_id){
+    echo $post_id;
+}
+add_action( 'manage_posts_custom_column', 'coldemo_post_column_data', 10, 2 );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
