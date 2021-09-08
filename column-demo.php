b1465313515e37e71a0720595ef183c720f2efba
Mahadi001 <sadat.himel@gmail.com>
2021-09-08 17:19:50 +0600

    update

--- column-demo.php
+++ column-demo.php
@@ -56,27 +56,39 @@ function coldemo_sortable_column( $columns ) {
 }
 add_filter( 'manage_edit-post_sortable_columns', 'coldemo_sortable_column' );
 
-// function coldemo_set_word_count(){
-//     $posts = get_posts( array(
+// function coldemo_set_word_count() {
+//     $posts = get_posts( [
 //         'posts_per_page' => -1,
-//         'post_type' => 'post'
-//     ) );
-//     foreach($posts as $p){
+//         'post_type'      => 'post',
+//         'post_status'    => 'any',
+//     ] );
+//     foreach ( $posts as $p ) {
 //         $content = $p->post_content;
-//         $wordn = str_word_count(strip_tags($content));
+//         $wordn   = str_word_count( strip_tags( $content ) );
 //         update_post_meta( $p->ID, 'wordn', $wordn );
 //     }
 // }
-// add_action( 'init', 'coldemo_set_word_count');
+// add_action( 'init', 'coldemo_set_word_count' );
 
 function coldemo_sort_column_data( $wpquery ) {
-    if ( !is_admin() ) {  
+    if ( !is_admin() ) {
         return;
     }
     $orderby = $wpquery->get( 'orderby' );
     if ( 'wordn' == $orderby ) {
-        $wpquery->set('meta_key','wordn');
-        $wpquery->set('orderby','meta_value_num');
+        $wpquery->set( 'meta_key', 'wordn' );
+        $wpquery->set( 'orderby', 'meta_value_num' );
     }
 }
 add_action( 'pre_get_posts', 'coldemo_sort_column_data' );
+
+function coldemo_update_wordcount_on_save_post($post_id){
+    $p = get_post($post_id);
+        $content = $p->post_content;
+        $wordn   = str_word_count( strip_tags( $content ) );
+        update_post_meta( $p->ID, 'wordn', $wordn );
+}
+add_action( 'save_post', 'coldemo_update_wordcount_on_save_post' );
+
+
+
